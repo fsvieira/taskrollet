94f15eb37a558d0f6b53e7a0796270917d7a9092
Filipe Vieira <sv.filipe@gmail.com>
2016-10-14 18:49:29 +0000

    added new tags

--- riotjs/src/js/main.js
+++ riotjs/src/js/main.js
@@ -1,8 +1,15 @@
-require('./libs/material.min.js');
-require('./tags/app.tag.js');
-require('./tags/task.tag.js');
 
 var riot = require('riot');
 var App = require('./app.js');
 
-riot.mount('app', new App());
+require('./libs/material.min.js');
+require('./tags/app.tag.js');
+require('./tags/tasks/tasks.tag.js');
+require('./tags/tasks/task.tag.js');
+require('./tags/tasks/notasks.tag.js');
+require('./tags/tasks/addtask.tag.js');
+require('./tags/tasks/taskmanager.tag.js');
+
+require('./tags/sprints/addsprint.tag.js');
+
+riot.mount('app', {app: new App()});
