18a3c70b2ee740fa15a6b0835bca971f2f09d1ab
Filipe Vieira <sv.filipe@gmail.com>
2020-04-02 16:53:41 +0100

    make editor bigger

--- src/components/TaskEditor.js
+++ src/components/TaskEditor.js
@@ -17,7 +17,12 @@ import { useActiveTags } from "../db/tasks/hooks";
 import TextInput from 'react-autocomplete-input';
 import 'react-autocomplete-input/dist/bundle.css';
 
-export default function TaskEditor({ task, onSave }) {
+export default function TaskEditor({
+  task,
+  onSave,
+  width = "100%",
+  height = "8em"
+}) {
 
   const [value, setValue] = useState(task ? task.description : "");
   const { tags } = useActiveTags();
@@ -78,13 +83,14 @@ export default function TaskEditor({ task, onSave }) {
     <Card
       interactive={true}
       elevation={Elevation.TWO}
+      style={{ height: "100%" }}
     >
       <TextInput
         options={newTags}
         trigger="#"
         offsetY={-50}
         offsetX={15}
-        style={{ width: "100%", height: "8em" }}
+        style={{ width, height }}
         value={value}
         onChange={setValue}
         placeholder={" Write here the task description, use # to add #tags!!"}
