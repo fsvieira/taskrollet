969a2e565f86dfbd9c1ea92ce8c462a4548a51f6
Filipe Vieira <sv.filipe@gmail.com>
2020-06-16 00:33:28 +0100

    fix languages

--- src/i18n.js
+++ src/i18n.js
@@ -22,6 +22,7 @@ i18n
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         fallbackLng: 'en',
+        whitelist: ["en", "pt"],
         debug: true,
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
